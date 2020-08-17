98023cb68a360a13d37a790f974f5a3e9e06f552
Administrator <Administrator@DESKTOP-K8EIRUA>
2020-08-17 18:20:01 +0900

    8월 17일

---
+++ spring_hello/src/main/java/com/wang/board/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.wang.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value = "/board")
+	public String main(Model model) {
+		return "/board/board";
+	}
+}
